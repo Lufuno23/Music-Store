83dc88f96caa94e93fead2f66e29f9a520943261
Lufuno23 <90466168+Lufuno23@users.noreply.github.com>
2022-09-19 13:18:08 +0200

    Add files via upload

---
+++ MusicStoreDbInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class MusicStoreDbInitializer
+        :System.Data.Entity.DropCreateDatabaseAlways<MusicStoreDB>
+    {
+        protected override void Seed(MusicStoreDB context)
+        {
+            context.Artists.Add(new Artist { Name = "Hugh Masekela" });
+            context.Genres.Add(new Genre { Name = "Jazz" });
+            context.Albums.Add(new Album
+            {
+                Artist = new Artist { Name = "Mdu" },
+                Genre = new Genre { Name = "Kawito" },
+                Price = 9.99m,
+                Title = "Mazola"
+            });
+
+
+
+            base.Seed(context);
+        }
+        }
+    }
